e9227f746178b58301683c34cb039c0f1443b1f7
Akpur Bibiana <akpurbibi@gmail.com>
2025-03-14 01:20:15 +0100

    first hero commit

--- landingpage/src/App.jsx
+++ landingpage/src/App.jsx
@@ -12,6 +12,20 @@ const App = () => {
         <h1 className='text-black-800 font-extrabold'>Landing</h1>
         <button className='bg-orange-200 text-black px-10 py-1 rounded-md font-medium'>Buy now</button>
       </header>
+
+      {/* HERO */}
+<div className='flex gap-8 bg-orange-200 h-screen items-center justify-center'>
+  <div className='w-[40%]'>
+    <h2>Introduce Your Product Quickly & Effectively</h2>
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus 
+    mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim</p>
+  </div>
+  <div className='w-[60%]'>
+    <img src='/heroimage.svg' className='w-[70%] h-[350px]'/>
+  </div>
+
+</div>
+
     </div>
   )
 }
